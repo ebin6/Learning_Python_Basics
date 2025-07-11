30d7393a941ed5dad1d27a7242f5e22abb44f0f3
ebin6 <ebin.oneteam@gmail.com>
2025-07-11 14:56:43 +0530

    Basic programs with for loop

---
+++ for_loop_pgms.py
@@ -0,0 +1,18 @@
+words=["Apple",'Kochi','ant','accer']
+
+"""
+for w in words:
+  if w.lower()[0]=='a':   
+    print(w)
+"""
+
+
+"""for w in words:
+  if w.lower()[0] in "aeiou":
+    print(w)"""
+
+for r in range(1, 4):
+  for j in range(1, r + 1):
+    print(j, end=" ")
+
+  print("")
