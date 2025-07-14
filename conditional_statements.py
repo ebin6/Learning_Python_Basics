e5bee1889d8c4444f79b841ae526bce32fc2ddfc
ebin6 <ebin.oneteam@gmail.com>
2025-07-14 14:23:03 +0530

    while loop intro

--- conditional_statements.py
+++ conditional_statements.py
@@ -39,21 +39,25 @@ if payment_status:
         print("Please increase your model exam marks")
 else:
     print("Please complete your payment")'''
+while True:
+    a=int(input("Enter the first number : "))
+    b=int(input("Enter the second number : "))
+    print("Please enter your choice :\n 1.Addition \n 2.Substarction\n3.Multiplication\n4.Division")
+    choice=int(input("Enter your choice 1,2,3,4 : "))
+    if choice==1:
+        print(a+b)
+    elif choice==2:
+        print(a-b)
+    elif choice==3:
+        print(a*b)
+    elif choice==4:
+        print(a/b)
+    else:
+        print("Please enter a valid input from choices given")
+    c=input("Do you wish to continue (yes/no) ?")
+    if c.lower()!="yes":
+        break
 
-a=int(input("Enter the first number : "))
-b=int(input("Enter the second number : "))
-print("Please enter your choice :\n 1.Addition \n 2.Substarction\n3.Multiplication\n4.Division")
-choice=int(input("Enter your choice 1,2,3,4 : "))
-if choice==1:
-    print(a+b)
-elif choice==2:
-    print(a-b)
-elif choice==3:
-    print(a*b)
-elif choice==4:
-    print(a/b)
-else:
-    print("Please enter a valid input from choices given")
 
 
 
---
+++ while_loop_intro.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the number : "))
+
+f=count=1
+while count<=num:
+    f=f*count
+    count+=1
+print(f)
+
+
