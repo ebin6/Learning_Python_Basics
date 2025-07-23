3ed7e24eb2ec23804ade7745be6d572afd2c4b02
ebin6 <ebin.oneteam@gmail.com>
2025-07-23 12:50:29 +0530

    ToDo Project version 1 with add , update functions

--- ToDoProgram.py
+++ ToDoProgram.py
@@ -11,13 +11,22 @@ def AddTask(task):
 
 def UpdateTask(task_id):
     global tasks
-    my_task=tasks.get(task_id)
+    my_task=tasks.get(task_id,"Tasks not found")
+    try:
+        my_task['status']=not my_task['status']
+        print("Task updated successfully")
+    except Exception as e:
+        print(e)
+        print("Failed to updated task")
+
 
 def RemoveTask(id):
     pass
 
 def ShowTask():
-    print(tasks)
+    print("Tasks")
+    for k in tasks:
+        print(f"{k} {tasks[k]['tasks']} - ","Done" if tasks[k]['status'] else "Incomplete")
 
 while True:
     print("ToDo Tasks")
@@ -25,8 +34,10 @@ while True:
     choice=int(input("Enter your choice : "))
     match choice:
         case 1:
-            task=input("Enter the task : ")
-            AddTask(task)
+            task_count=int(input("How many task do you wish to enter : "))
+            for k in range(task_count):
+                task=input("Enter the task : ")
+                AddTask(task)
         case 2:
             task_id=int(input("Enter the task id : "))
             UpdateTask(task_id)
